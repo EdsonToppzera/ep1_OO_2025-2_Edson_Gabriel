ff3e53b355e58b56ee83beaf75b8d0ebc4e880f6
Edson Gabriel <edsongabrielbb@gmail.com>
2025-09-28 19:29:01 -0300

    teste teste

---
+++ src/classes/PlanoDeSaude.java
@@ -0,0 +1,13 @@
+package classes;
+import java.util.List;
+import java.util.ArrayList;
+
+public class PlanoDeSaude {
+    private String nome;
+    private boolean pagaInternacao;
+    private Map<Especialidade, float> descontos;
+
+    //construtores
+    public Des
+
+}
